85aa87d66b06c463393b5f1be45a77552268ff0e
AkaVortex <akanksha24d@gmail.com>
2025-06-13 22:42:05 +0530

    Hello Coder

---
+++ PSUAdavancePy/Day_1/Time_Prgm.py
@@ -0,0 +1,17 @@
+class Time:
+    def __init__(self,year,month):
+        self.year = year + month // 12
+        self.month = month % 12
+    def __add__(self,other):
+        total_y = self.year + other.year
+        total_m = self.month + other.month
+        if total_m >= 12:
+            total_y = total_y + total_y // 12
+            total_m = total_m % 12
+        return Time(total_y , total_m)
+    def __repr__(self):
+        return f"{self.year} years and {self.month} months"
+time1 = Time(21,54)
+time2 = Time(18,25)
+time = time1 + time2
+print(time)
